f43f7026e68dde4b905769188d0638a114f3b014
Daniel Zlotin <zlotindaniel@gmail.com>
2018-08-21 15:54:15 +0300

    update scripts

--- scripts/wix-publish.js
+++ scripts/wix-publish.js
@@ -1,4 +1,3 @@
-/*eslint-disable no-undef */
 require('shelljs/global');
 const semver = require('semver');
 
@@ -8,32 +7,15 @@ releaseVersion.patch += 100;
 releaseVersion.version = `${releaseVersion.major}.${releaseVersion.minor}.${releaseVersion.patch}`;
 releaseVersion.raw = releaseVersion.version;
 
+exec(`scripts/bump-oss-version.js ${releaseVersion}`);
+
 // -------- Generating Android Artifacts with JavaDoc
 if (exec('./gradlew clean :ReactAndroid:installArchives').code) {
   echo('Couldn\'t generate artifacts');
   exit(1);
 }
 
-exec(`scripts/bump-oss-version.js ${releaseVersion}`);
-
-// undo uncommenting javadoc setting
-exec('git checkout ReactAndroid/gradle.properties');
-
-echo('Generated artifacts for Maven');
-
-let artifacts = ['-javadoc.jar', '-sources.jar', '.aar', '.pom'].map((suffix) => {
-  return `react-native-${releaseVersion}${suffix}`;
-});
-
-artifacts.forEach((name) => {
-  if (!test('-e', `./android/com/facebook/react/react-native/${releaseVersion}/${name}`)) {
-    echo(`file ${name} was not generated`);
-    exit(1);
-  }
-});
-
 exec('npm publish');
 echo(`Published to npm ${releaseVersion}`);
 
 exit(0);
-/*eslint-enable no-undef */

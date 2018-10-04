2ca53f7015dcfcd87dd9363637eca95395c82bed
Daniel Zlotin <zlotindaniel@gmail.com>
2018-10-04 16:25:50 +0300

    fixed build.gradle part 1

---
+++ scripts/wix-publish.js
@@ -0,0 +1,15 @@
+require('shelljs/global');
+const semver = require('semver');
+
+let currentVersion = require('../package.json').version;
+let releaseVersion = semver.parse(currentVersion);
+releaseVersion.patch += 100;
+releaseVersion.version = `${releaseVersion.major}.${releaseVersion.minor}.${releaseVersion.patch}`;
+releaseVersion.raw = releaseVersion.version;
+
+echo(`Building ${releaseVersion}, make sure you are using the private artifactory credentials`);
+exec(`scripts/bump-oss-version.js ${releaseVersion}`);
+exec('./gradlew clean :ReactAndroid:installArchives');
+echo(`Publishing to npm ${releaseVersion}...`);
+exec('npm publish');
+echo(`Published to npm ${releaseVersion}`);

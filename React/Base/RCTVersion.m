42b3dae4a91a1700fe4be311bd8c29e812250315
Rotem M <rotemm@wix.com>
2018-11-12 15:27:04 +0200

    [0.57.104] Bump version numbers

--- React/Base/RCTVersion.m
+++ React/Base/RCTVersion.m
@@ -22,7 +22,7 @@ static void __makeVersion()
   __rnVersion = @{
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(57),
-                  RCTVersionPatch: @(4),
+                  RCTVersionPatch: @(104),
                   RCTVersionPrerelease: [NSNull null],
                   };
 }

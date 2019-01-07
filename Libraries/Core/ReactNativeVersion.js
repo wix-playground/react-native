e53f077c97a70f9506f18e596ab2c06328ddfc88
Amit Davidi <amit.d4vidi@gmail.com>
2019-01-07 16:58:35 +0200

    [0.51.2900] Bump version numbers

--- Libraries/Core/ReactNativeVersion.js
+++ Libraries/Core/ReactNativeVersion.js
@@ -15,6 +15,6 @@
 exports.version = {
   major: 0,
   minor: 51,
-  patch: 2800,
+  patch: 2900,
   prerelease: null,
 };
--- React/Base/RCTVersion.h
+++ React/Base/RCTVersion.h
@@ -12,6 +12,6 @@
 #define RCT_REACT_NATIVE_VERSION @{ \
   @"major": @(0), \
   @"minor": @(51), \
-  @"patch": @(2800), \
+  @"patch": @(2900), \
   @"prerelease": [NSNull null], \
 }

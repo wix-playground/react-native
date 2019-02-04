2b50b0975e500a61571f350057bd851cff731608
Rotem M <rotemm@wix.com>
2019-02-05 00:05:14 +0200

    Fixed release script, will not commit and push to git on failed builds

--- scripts/wix-publish.js
+++ scripts/wix-publish.js
@@ -1,15 +1,17 @@
 require('shelljs/global');
 const semver = require('semver');
-
+const release = require('./bump-oss-version');
 let currentVersion = require('../package.json').version;
 let releaseVersion = semver.parse(currentVersion);
 releaseVersion.patch += 100;
 releaseVersion.version = `${releaseVersion.major}.${releaseVersion.minor}.${releaseVersion.patch}`;
 releaseVersion.raw = releaseVersion.version;
 
+set('-e');
 echo(`Building ${releaseVersion}, make sure you are using the private artifactory credentials`);
-exec(`scripts/bump-oss-version.js ${releaseVersion}`);
+release.bumpVersion(releaseVersion.version);
 exec('./gradlew :ReactAndroid:installArchives');
 echo(`Publishing to npm ${releaseVersion}...`);
 exec('npm publish');
-echo(`Published to npm ${releaseVersion}`);
+release.commitAndPush(releaseVersion.version);
+echo(`##teamcity[version: ${releaseVersion}]`);
